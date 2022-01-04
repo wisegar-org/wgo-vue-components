72214e7f894934995cf9098ea9f24507c201da47
Yariel Rodriguez <info@wisegar.org>
2022-01-04 16:07:32 -0500

    0.0.1-22

--- src/boot/register.js
+++ src/boot/register.js
@@ -2,11 +2,11 @@
 // file: /src/boot/register-my-component.js
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
-const vue_1 = tslib_1.__importDefault(require("vue"));
-const MyButton_vue_1 = tslib_1.__importDefault(require("../components/MyButton/MyButton.vue"));
-const MyButtom_vue_1 = tslib_1.__importDefault(require("../components/MyButtom/MyButtom.vue"));
-const WGODialog_vue_1 = tslib_1.__importDefault(require("../components/WGODialog/WGODialog.vue"));
-const WGOLoading_vue_1 = tslib_1.__importDefault(require("../components/WGOLoading/WGOLoading.vue"));
+const vue_1 = (0, tslib_1.__importDefault)(require("vue"));
+const MyButton_vue_1 = (0, tslib_1.__importDefault)(require("../components/MyButton/MyButton.vue"));
+const MyButtom_vue_1 = (0, tslib_1.__importDefault)(require("../components/MyButtom/MyButtom.vue"));
+const WGODialog_vue_1 = (0, tslib_1.__importDefault)(require("../components/WGODialog/WGODialog.vue"));
+const WGOLoading_vue_1 = (0, tslib_1.__importDefault)(require("../components/WGOLoading/WGOLoading.vue"));
 // we globally register our component with Vue
 exports.default = ({ app }) => {
     vue_1.default.component("my-button", MyButton_vue_1.default);
