114e8b82f2bfa4c396b8486f7dc64081af60851b
Yariel Rodriguez <info@wisegar.org>
2022-01-07 10:20:58 -0500

    Add export models

--- src/models/index.js
+++ src/models/index.js
@@ -2,3 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 (0, tslib_1.__exportStar)(require("./IWGODialogOptions"), exports);
+(0, tslib_1.__exportStar)(require("./IWGOInputDateOptions"), exports);
+(0, tslib_1.__exportStar)(require("./IWGOInputOptions"), exports);
+(0, tslib_1.__exportStar)(require("./IWGOSelectableObject"), exports);
+(0, tslib_1.__exportStar)(require("../components/WGOExpandableList/models"), exports);
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,5 @@
 export * from "./IWGODialogOptions";
+export * from "./IWGOInputDateOptions";
+export * from "./IWGOInputOptions";
+export * from "./IWGOSelectableObject";
+export * from "../components/WGOExpandableList/models";
